85f9978ae12d1a4006a7d31906015134fb2bfb33
GrishaOc <mr3p@yandex.ru>
2024-01-04 18:54:48 +0300

    second

--- scripts/index.js
+++ scripts/index.js
@@ -3,27 +3,29 @@ const cardsTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placeContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function createCards(link,title) {
+function createCards(link,name) {
     const cardElement = cardsTemplate.querySelector('.card').cloneNode(true);
 
-    cardElement.querySelector('.card__title').textContent = title;
+    cardElement.querySelector('.card__title').textContent = name;
 
     const cardImage = cardElement.querySelector('.card__image');
     cardImage.setAttribute('src', link);
-    cardImage.setAttribute('alt', title);
+    cardImage.setAttribute('alt', name);
 
-    cardElement.querySelector('.card__delete-button').addEventListener('click',function deleteCards() {
-        cardElement.remove();
-    });
+    const delBut = cardElement.querySelector('.card__delete-button');
+    delBut.addEventListener('click',() => deleteCards(cardElement));
 
     return cardElement;
 };
 // @todo: Функция удаления карточки
+function deleteCards(cardElement) {
+    cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
 function showcards(cards) {
     cards.forEach(item => {
-        const cardElement = createCards(item.link, item.title);
+        const cardElement = createCards(item.link, item.name);
         placeContainer.append(cardElement);
     });
 }
