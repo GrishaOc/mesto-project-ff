1c5ba04d2ae0ef71f5f2a0069dcf0478fd812e92
QWELI <mr3p@yadnex.ru>
2024-01-06 18:17:48 +0300

    3

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const cardsTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placeContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function createCards(link,name) {
+function createCards(link,name,deleteCards) {
     const cardElement = cardsTemplate.querySelector('.card').cloneNode(true);
 
     cardElement.querySelector('.card__title').textContent = name;
@@ -11,7 +11,7 @@ function createCards(link,name) {
     const cardImage = cardElement.querySelector('.card__image');
     cardImage.setAttribute('src', link);
     cardImage.setAttribute('alt', name);
-
+    
     const delBut = cardElement.querySelector('.card__delete-button');
     delBut.addEventListener('click',() => deleteCards(cardElement));
 
@@ -25,7 +25,7 @@ function deleteCards(cardElement) {
 // @todo: Вывести карточки на страницу
 function showcards(cards) {
     cards.forEach(item => {
-        const cardElement = createCards(item.link, item.name);
+        const cardElement = createCards(item.link, item.name, deleteCards);
         placeContainer.append(cardElement);
     });
 }
